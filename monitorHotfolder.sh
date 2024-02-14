629945aebfb90ac4146d5a6142d09b38deba3c4d
Tilo Neumann <tilo.neumann@gbv.de>
2024-02-14 09:34:13 +0100

    DMA-240 adds a simple monitor for the hotfolder

---
+++ monitorHotfolder.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+startTime=$(date +"%Y-%m-%d_%H-%M-%S")
+hotfolderPath="/opt/digiverso/viewer/hotfolder/"
+
+countFiles=`find $hotfolderPath -type f -print | wc -l`
+startFiles=$countFiles
+
+# clear line
+echo -e "\033[2K"
+
+while [ $countFiles != 0 ]
+do
+  countFiles=`find $hotfolderPath -type f -print | wc -l`
+  echo -ne "Dateien im Hotfolder: "$countFiles'\r'
+  sleep 5s
+done
+
+if [ $# -eq 0 ]
+then
+  bashCall="monitor starts with no param"
+else
+  bashCall=$1
+
+  case $1 in
+
+    "restartTom")
+      silentRestartTom
+      ;;
+
+    "importChanged")
+      silentImportChanged
+      ;;
+
+    *)
+      bashCall="unknown: "$1
+      ;;
+
+  esac
+fi
+
+endTime=$(date +"%Y-%m-%d_%H-%M-%S")
+
+recipient="goobi-viewer-support@lists.gbv.de"
+#recipient="tilo.neumann@gbv.de"
+sender="no-reply@gbv.de"
+subject="[goobi-viewer] [Denkmalatlas] Hotfolder Status"
+body="<p>Number of Files in Hotfolder when Monitor starts: "$startFiles"</br>"
+body=$body"Start Time: "$startTime"</br>"
+body=$body"Bash Call: "$bashCall"</br>"
+body=$body"End Time: "$endTime"</p>"
+
+# For HTML emails, add the following line
+MIME="MIME-Version: 1.0\nContent-Type: text/html\n"
+
+echo -e "To: $recipient\nFrom: $sender\nSubject: $subject\n$MIME\n\n$body" | sendmail -t
