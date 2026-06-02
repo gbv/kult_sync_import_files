baba655c06984bfa9c87a48e54d11fc86ea63ef3
Tilo Neumann <tilo.neumann@gbv.de>
2026-06-02 09:01:04 +0200

    updates sync to new nfis export

--- importChangedData.sh
+++ importChangedData.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# get import files from wsl
-/usr/bin/php /opt/digiverso/kult_dma_object_importer/run.php full 10000000 cold
+# get import files from nfis
+denkxport --limit=100 --no-purge --skip-images
 
 # clean up import files, sync with last imports
 bash /opt/digiverso/kult_sync_import_files/syncFiles.sh
 
-# move images to hotfolder
-find /opt/digiverso/viewer/coldfolder/ -name "*_downloadimages" -type d -print0 | xargs -0 -I % mv % /opt/digiverso/viewer/hotfolder/
+# move images to hotfolder and
+#movemedia
+
+#set right permissions
+#chownmedia
 
 # move import files to hotfolder
-find /opt/digiverso/viewer/coldfolder/ -name "*.xml" -print0 | xargs -0 -I % mv % /opt/digiverso/viewer/hotfolder/
+movedenkx
 
 # monitor hotfolder and start new import when last one is done
-bash /opt/digiverso/kult_sync_import_files/monitorHotfolder.sh importChanged
+#bash /opt/digiverso/kult_sync_import_files/monitorHotfolder.sh importChanged
 
 # monitor hotfolder and restart tomcat when last import is done
-bash /opt/digiverso/kult_sync_import_files/monitorHotfolder.sh restartTom
+#bash /opt/digiverso/kult_sync_import_files/monitorHotfolder.sh restartTom
